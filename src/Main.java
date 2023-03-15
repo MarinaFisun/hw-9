a2a0589ad2bf35ad0159b95dcb4bc43e7604fe6c
Marina <m-fisun23@yandex.ru>
2023-03-15 13:34:08 +0300

    1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+    public static void task1() {
+        System.out.println("Task 1");
+        int[] arr = new int[3];
+        arr [0] = 1;
+        arr [1] = 2;
+        arr [2] = 3;
+
+        double [] secondArr = {1.57d, 7.654d, 9.986d};
+
+        int [] random = {14, 75, 24, 8, 3, 0};
+    }
+    public static void task2() {
+        System.out.println("Task 2");
+        int[] arr = new int[3];
+        arr [0] = 1;
+        arr [1] = 2;
+        arr [2] = 3;
+        double [] secondArr = {1.57d, 7.654d, 9.986d};
+        int [] random = {14, 75, 24, 8, 3, 0};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr [i]);
+            if (i != arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < secondArr.length; i++) {
+            System.out.print(secondArr [i]);
+            if (i != secondArr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+        for (int i = 0; i < random.length; i++) {
+            System.out.print(random [i]);
+            if (i != random.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
